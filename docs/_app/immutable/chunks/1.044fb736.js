12cf9052cacb4963b2f7051ec79447a9cee918cc
kntryer <1162676656@qq.com>
2023-03-22 18:49:20 +0800

    fix

--- docs/_app/immutable/chunks/1.044fb736.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.bbb27885.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,6 @@ const config = {
         }),
         paths: {
             base: dev ? "" : "/kevinrocco.github.io",
-			relative: false
         },
     },
 };
